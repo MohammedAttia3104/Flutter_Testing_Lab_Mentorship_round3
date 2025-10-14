ee538aded8b758d4440110789d484178f0cb881f
MohammedAttia3104 <attia3104@gmail.com>
2025-10-14 21:29:17 +0300

    Add tests for UserRegistrationForm
    - Implement widget tests to verify form validation and the submission process.
    - Test for empty field validation messages.
    - Test for successful form submission flow, including loading indicator and success message.

--- test/widgets/user_registration_form_test.dart
+++ test/widgets/user_registration_form_test.dart
@@ -1,7 +1,44 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_testing_lab/widgets/user_registration_form.dart';
 
 void main() {
-  test('TODO: Implement tests for user_registration_form.dart', () {
-    // TODO: Implement test
+  testWidgets('UserRegistrationForm validation and submit', (tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(home: Scaffold(body: UserRegistrationForm())),
+    );
+
+    await tester.tap(find.text('Register'));
+    await tester.pump();
+
+    expect(find.text('Please enter your full name'), findsOneWidget);
+    expect(find.text('Please enter your email'), findsOneWidget);
+    expect(find.text('Please enter a password'), findsOneWidget);
+    expect(find.text('Please confirm your password'), findsOneWidget);
+
+    await tester.enterText(find.byType(TextFormField).at(0), 'Mohamed');
+    await tester.enterText(
+      find.byType(TextFormField).at(1),
+      'attia3104@gmail.com',
+    );
+    await tester.enterText(find.byType(TextFormField).at(2), 'Password123@@');
+    await tester.enterText(find.byType(TextFormField).at(3), 'Password123@@');
+
+    await tester.tap(find.text('Register'));
+    await tester.pump();
+
+    ///loading
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    ///error
+    expect(
+      find.text('Please fix the errors in red before submitting.'),
+      findsNothing,
+    );
+
+    await tester.pumpAndSettle();
+
+    ///success
+    expect(find.text('Registration successful!'), findsOneWidget);
   });
 }
